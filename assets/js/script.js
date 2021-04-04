6aa0ef87c9e6bc3b971292a002db8fc3b034d873
Matthew Parker <matt.parker1007@gmail.com>
2021-04-04 16:13:45 -0500

    first line of today's weather, including icon, renders when a city is searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
+// user enters/clicks city name:
+//  * fetch current weather and display
+//  * fetch 5-day forecast and display
+//  * add the city to local storage
+//  * append city to recents
+
 var key = "12bc64a3e0930862a15da754f74d5af8";
 
-
+var forecastEl = $('.forecast');
+var searchFormEl = $('#searchForm')
+var cityInputEl = $('#cityInput');
 
 function getCurrentWeather(city) {
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
@@ -8,16 +16,29 @@ function getCurrentWeather(city) {
   fetch(url)
     .then(function (response)  {
       // console.log(response);
+      if(response.status === 404){
+        console.log('404');
+        return;
+      }
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      renderCurrentWeather(data);
     });
 }
-// user enters/clicks city name:
-//  * fetch current weather and display
-//  * fetch 5-day forecast and display
-//  * add the city to local storage
-//  * append city to recents
 
-getCurrentWeather("Houston");
+searchFormEl.on('submit', function(event) {
+  event.preventDefault();
+  var cityInput = cityInputEl.val();
+  getCurrentWeather(cityInput);
+})
+
+function renderCurrentWeather(retrievedData){
+  var date = moment().format("M[/]D[/]YYYY");
+  var iconCode = retrievedData.weather[0].icon;
+  var iconEl = $('#icon-0');
+  iconEl.attr('src', `http://openweathermap.org/img/wn/${iconCode}.png`)
+  
+  $('.cityName').text(retrievedData.name + ' ' + date + ' ');
+}
