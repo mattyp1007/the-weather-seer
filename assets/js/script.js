1b1ab7700a12bd567d28ea7257f903ca2b7ab6a1
Matthew Parker <matt.parker1007@gmail.com>
2021-04-05 20:24:21 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,44 @@
-// user enters/clicks city name:
-//  * fetch current weather and display
-//  * fetch 5-day forecast and display
-//  * add the city to local storage
-//  * append city to recents
-
+// don't look at the line below
 var key = "12bc64a3e0930862a15da754f74d5af8";
-
+// true if the search is not on the list
 var newSearch = false;
+// element selectors
 var forecastEl = $('.forecast');
 var searchFormEl = $('#searchForm')
 var cityInputEl = $('#cityInput');
 var cityListItems = $('#recentsList').children();
 var searchAlertEl = $('#searchAlert');
 
-
 // get the saved searches
 var cities = JSON.parse(localStorage.getItem('cities'));
 if(!cities) { 
   cities = []; 
 }
-console.log(cities);
+// render the recent searches buttons
 for(var i = 0; i < cities.length; i++){
   createButton(cities[i]);
 }
 
 function createButton(buttonText) {
+  // create a button and add it to the list
   var recentsListEl = $('#recentsList');
   var newButton = document.createElement('button');
   $(newButton).attr('type', 'button');
   $(newButton).addClass('list-group-item list-group-item-action list-group-item-dark rounded text-center mt-3 py-1 cityButton');
   $(recentsListEl).append(newButton);
   $(newButton).text(buttonText);
-  // add to array and save to local storage
   
+  // add listener of clicks on the button
   $(newButton).click( function(event) {
     newSearch = false;
     getCoords($(newButton).text());
-    console.log('Getting weather for '+$(newButton).text());
   })
 }
+
+// renders all the weather data
 function renderWeatherData(retrievedData, city){
   // Current Weather //////////////////////////////////////
+
   var date = moment().format("M[/]D[/]YYYY");
   var iconCode = retrievedData.current.weather[0].icon;
   var iconEl = $('#icon-0');
@@ -65,6 +63,7 @@ function renderWeatherData(retrievedData, city){
   uvEl.text(uvIndex);
 
   // 5-Day Forecast ///////////////////////////////////////
+  // render the data for a forecast card, loop 5 times
   for(var i = 1; i < 6; i++){
     var day = moment().add(i, 'd').format("M[/]D[/]YYYY");
     var dateEl = $('#date-'+i);
@@ -86,12 +85,11 @@ function renderWeatherData(retrievedData, city){
     humidity = retrievedData.daily[i].humidity;
     humidityEl.text(humidity + ' %');
   }
-  console.log('showing forecast')
+  // make the content visible
   $(forecastEl).removeClass('invisible');
 }
 
-
-// get weather data with a call to the One Call API
+// get weather data with a call to the OneCall API
 function getWeather(x, y, cityName) {
   var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${x}&lon=${y}&exclude=minutely,hourly,alerts&appid=${key}&units=imperial`
   fetch(url)
@@ -106,6 +104,7 @@ function getWeather(x, y, cityName) {
       }
     })
 }
+
 // get city coordinates with a call to the Current Weather API
 function getCoords(city) {
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
@@ -114,13 +113,11 @@ function getCoords(city) {
       return response.json();      
     })
     .then(function (data) {
-      // continue if city found
       try{
         var lat = data.coord.lat;
         var lon = data.coord.lon;
-        getWeather(lat, lon, city);
-        $(searchAlertEl).addClass('d-none');
-        console.log('hiding alert');
+        getWeather(lat, lon, city); // call OneCall with retrieved coordinates
+        $(searchAlertEl).addClass('d-none'); // good input, so hide alert
       }
       // if error, remove the city from the array and display alert & hide forecast
       catch{
@@ -135,12 +132,11 @@ function getCoords(city) {
 searchFormEl.on('submit', function(event) {
   event.preventDefault();
   var cityInput = cityInputEl.val().trim();
-  
   // check if this search has been done before
   if(!(cities.includes(cityInput))){
     newSearch = true;
     cities.push(cityInput);
     localStorage.setItem('cities', JSON.stringify(cities));
   }
-  getCoords(cityInput);
+  getCoords(cityInput); // get the coords, then get weather data
 })
